ef31c1f8ea5e0535cb1de1df5f77cd1de4a907fd
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2024-04-25 07:14:08 +0900

    edit b_step2 def heap_push, def heap_pop

--- b_step2.py
+++ b_step2.py
@@ -1,29 +1,49 @@
-def heap_push(arr, arr_g, previous_w):
-    for i in range(len(arr_g)):
-        arr_g[i][1] += previous_w
-        arr.append(arr_g[i])
-
-        now_node = len(arr)-1
-
-        while now_node > 0:
-            parent_node = (now_node-1)//2
-            if arr[now_node][1] < arr[parent_node][1]:
-                arr[now_node], arr[parent_node] = arr[parent_node], arr[now_node]
+def heap_push(arr):
+    now_node = len(arr)-1
+    while now_node > 0:
+        parent_node = (now_node - 1) // 2
+        if step[arr[now_node]] <= step[arr[parent_node]]:
+            arr[now_node], arr[parent_node] = arr[parent_node], arr[now_node]
+            now_node = parent_node
+        else:
+            break
+def heap_pop(arr):
+    now_node = 0
+    while now_node < len(arr):
+        child_node1 = 2 * now_node + 1
+        child_node2 = 2 * now_node + 2
+        if child_node1 > len(arr) - 1:
+            break
+        if child_node2 > len(arr) - 1:
+            if step[arr[now_node]] > step[arr[child_node1]]:
+                arr[now_node], arr[child_node1] = arr[child_node1], arr[now_node]
                 now_node = parent_node
             else:
-                 break
-
-    return arr
-
+                break
+        if child_node2 <= len(arr) - 1:
+            if step[arr[child_node1]] <= step[arr[child_node2]]:
+                if step[arr[now_node]] > step[arr[child_node1]]:
+                    arr[now_node], arr[child_node1] = arr[child_node1], arr[now_node]
+                    now_node = child_node1
+                else:
+                    break
+            else:
+                if step[arr[now_node]] > step[arr[child_node2]]:
+                    arr[now_node], arr[child_node2] = arr[child_node2], arr[now_node]
+                    now_node = child_node2
+                else:
+                    break
 
 n, m, s = map(int, input().split())
-
 s -= 1
+inf = 100100100100
 
 g = [[] for _ in range(n)]
 
-near_node = []
-shortest_path = []
+step = [inf]*n
+is_visited = [False]*n
+adj_nodes = []
+path = []
 
 for _ in range(m):
     a, b, w = map(int, input().split())
@@ -31,16 +51,64 @@ for _ in range(m):
     b -= 1
     g[a].append([b, w])
 
-previous_w = 3
-start_node = s
+now_v = s
+
+step[now_v] = 0
+is_visited[now_v] = True
+path.append(now_v)
+
+now_step = step[now_v]
+
+# 隣接nodeをリストに追加、最短nodeを先頭にソート
+for i in range(len(g[now_v])):
+    adj_node = g[now_v][i][0]
+    if not is_visited[adj_node]:
+        adj_nodes.append(adj_node)
+        step[adj_node] = min(step[adj_node], g[now_v][i][1] + now_step)
+
+        heap_push(adj_nodes)
+
+# 最短nodeを取得、最短nodeへ移動、pathに追加
+now_v = adj_nodes.pop(0)
+is_visited[now_v] = True
+path.append(now_v)
+
+# 初期nodeから現在nodeまでのstep
+now_step = step[now_v]
+
+# 最短nodeを先頭にソート
+adj_nodes = [adj_nodes.pop()] + adj_nodes
+heap_pop(adj_nodes)
+
+for i in range(len(g[now_v])):
+    adj_node = g[now_v][i][0]
+    if not is_visited[adj_node]:
+        adj_nodes.append(adj_node)
+        step[adj_node] = min(step[adj_node], g[now_v][i][1] + now_step)
+
+        heap_push(adj_nodes)
+
+
+now_v = adj_nodes.pop(0)
+is_visited[now_v] = True
+path.append(now_v)
+
+now_step = step[now_v]
+
+adj_nodes = [adj_nodes.pop()] + adj_nodes
+heap_pop(adj_nodes)
+
+print(adj_nodes)
+print(is_visited)
+print(step)
+print(path)
 
 #near_node = heap_push(near_node, g[start_node])
-near_node = heap_push(near_node, g[4], previous_w)
+#near_node = heap_push(near_node, g[4], previous_w)
 
+#print("----")
+#print(near_node)
+#print("----")
 
-print("----")
-print(near_node)
-print("----")
-
-for gi in g:
-    print(gi)
+#for gi in g:
+#    print(gi)
